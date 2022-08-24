b37bc659a05356cd8fa84d4802e9024491ede2a6
zenghuihuang <huihui127127@gmail.com>
2022-08-24 22:02:31 +0100

    added local values

--- stages/services/webserver-cluster/main.tf
+++ stages/services/webserver-cluster/main.tf
@@ -1,11 +1,11 @@
-provider "aws"{
-    region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
-module "webserver-staging"{
-    source = "../../../modules/services/webserver-cluster"
+module "webserver-staging" {
+  source = "../../../modules/services/webserver-cluster"
 
-    cluster_name = "webserver-stage"
-    instance_type = "t2.micro"
-    min_size = 2
-    max_size = 5
+  cluster_name  = "webserver-stage"
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 5
 }
